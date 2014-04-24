da77ceac442e029892cd9bbec9b7f5f4b8a3f9a3
Cristian <Cristian@PC_Casa>
2014-04-23 23:19:25 -0500

    Librerias de utilidades, como cifrar contraseñas, crear usario y login, enviar correos de una cuenta determindad, manejo de fechas, de un string a fecha java o al contrario con un formato establecido, si quieren cambiar el formato, consultar SimpleDateFormat

---
+++ MarketPlaceTourismEcologicEJB-war/src/java/co/edu/uniminuto/MarketPlaceTourismEcologicEJB/utils/LoginUtils.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package co.edu.uniminuto.MarketPlaceTourismEcologicEJB.utils;
+
+/**
+ * @author Cristian rodriguez    baruchneo@gmail.com
+ * @author Cristian Vargas      cvarga35@uniminuto.edu.co
+ * @author Yohan EspaNa         espanaqwe123@gmail.com
+ * @author Wilson Lombana       sonwil1525@gmail.com
+ * 
+ */
+public class LoginUtils 
+{
+    public String crearUsuario(String nombre, String Apellido)
+    {
+        return null;
+    }
+}
